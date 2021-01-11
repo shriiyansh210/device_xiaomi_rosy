7c2b040d45f7b85b659003ff34b879ffe33c62c5
shriiyansh210 <shriiyansh210@gmail.com>
2021-01-11 23:31:44 +0530

    bringup

--- lineage_rosy.mk
+++ lineage_rosy.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Pixel customization
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
@@ -21,7 +21,7 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_rosy
+PRODUCT_NAME := lineage_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
